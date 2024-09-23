a4ec31bc1e4ade363cf29687aaf6d43bd8bc4ef9
Alexandr Savitsky <savitsky.alexandr@mail.ru>
2024-09-24 00:33:20 +0300

    add func for clear names

---
+++ src/main_1.py
@@ -0,0 +1,21 @@
+def clear_names(file_name: str) -> list:
+    """Функция для очистки имён от лишних символов"""
+    new_names_list = list()
+    with open('data/' + file_name) as names_file:
+        names_list = names_file.read().split()
+        for name_items in names_list:
+            new_name = ''
+            for symbol in name_items:
+                if symbol.isalpha():
+                    new_name +=symbol
+            if new_name.isalpha():
+                new_names_list.append(new_name)
+    return new_names_list
+
+
+if __name__ == '__main_1__':
+    cleared_name = clear_names('names.txt')
+
+    for i in cleared_name:
+        print(i)
+
